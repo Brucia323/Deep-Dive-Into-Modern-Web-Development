0a1d6383ba84e4d581eb6a7ad9e7b91bbf80bfc5
ZZZCNY <1448517833@qq.com>
2022-02-26 13:37:36 +0800

    22.2.26

---
+++ router-tutorial/src/index.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Expenses from './routes/expenses'
+import Invoices from './routes/invoices'
+import Invoice from './routes/invoice'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />}>
+          <Route path='/expenses' element={<Expenses />} />
+          <Route path='/invoices' element={<Invoices />}>
+            <Route
+              index
+              element={
+                <main style={{ padding: '1rem' }}>
+                  <p>Select an invoice</p>
+                </main>
+              }
+            />
+            <Route path=':invoiceId' element={<Invoice />} />
+          </Route>
+          <Route
+            path='*'
+            element={
+              <main style={{ padding: '1rem' }}>
+                <p>There's nothing here!</p>
+              </main>
+            }
+          />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals()
